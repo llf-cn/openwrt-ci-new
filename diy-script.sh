22ea53607188ef176848a150f4f53ebda9e32d3e
llf-cn <61875916+llf-cn@users.noreply.github.com>
2025-08-09 23:15:57 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -7,9 +7,6 @@ sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generat
 # 添加 OpenClash 源
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >> feeds.conf.default
 
-# 修复 armv8 设备 xfsprogs 报错
-sed -i 's/TARGET_CFLAGS.*/TARGET_CFLAGS += -DHAVE_MAP_SYNC -D_LARGEFILE64_SOURCE/g' feeds/packages/utils/xfsprogs/Makefile
-
 # 修正 Makefile 路径
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's|\.\./\.\./luci.mk|$(TOPDIR)/feeds/luci/luci.mk|g' {}
 find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's|\.\./\.\./lang/golang/golang-package.mk|$(TOPDIR)/feeds/packages/lang/golang/golang-package.mk|g' {}
